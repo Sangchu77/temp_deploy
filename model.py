ef7b0c3dbe7ac6a35055e7d3e100631ed2e2df81
Sangchu <88162218+Sangchu77@users.noreply.github.com>
2024-06-16 17:55:24 +0900

    Update model.py

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ def recommend(user_ingredients):
     tfidf = TfidfVectorizer(token_pattern=r'[^|]+')
     tfidf_matrix = tfidf.fit_transform(recipe_df['Ingr2'])
 
-    user_ingredients_str = '|'.join(user_ingredients)
+    user_ingredients_str = '|'.join(list(user_ingredients))
     user_vector = tfidf.transform([user_ingredients_str])
 
     scores = cosine_similarity(tfidf_matrix, user_vector).flatten()
