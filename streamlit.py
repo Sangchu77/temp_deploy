71ba9a6488c02c63d60a1c4cfee1afacf42eab2c
Sangchu <88162218+Sangchu77@users.noreply.github.com>
2024-06-16 17:47:07 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -18,6 +18,7 @@ if st.button('분석'):
     temp = recommend([user_input])
 
     for i, row in temp.iterrows():
-       st.write(f"추천 레시피: {row['best_name']}, {row['Ingr2']}")
+       st.write(f"추천 레시피: {row['best_name']}")
+       st.write(f"사용 재료: {row['Ingr2']}")
        recipe_url = f"https://www.10000recipe.com/recipe/{row['index']}"
        st.markdown(f"[레시피 보러가기]({recipe_url})")
